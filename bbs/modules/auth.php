0f48df1ff4d14da92b73bf305640e514e8f20042
glx142857 <55825956+glx142857@users.noreply.github.com>
2021-06-04 21:34:30 +0800

    Add files via upload

---
+++ bbs/modules/auth.php
@@ -0,0 +1,41 @@
+<?php 
+	
+	require_once('./includes/db.php');
+
+	function login($username, $password) {
+
+		$sql = "select * from t_user where username = '$username' and password = '$password'";
+
+		$user = exe_sql($sql);
+
+		if (empty($user)) {
+			
+			return false;
+
+		} else {
+
+			return true;
+
+		}
+
+	}
+
+	function valid_username($username) {
+
+		$sql = "select * from t_user where username = '$username'";
+
+		$user = exe_sql($sql);
+
+		return empty($user) ? true : false;
+
+	}
+
+	function register($username, $password) {
+
+		$sql = "insert into t_user values(0, '$username', '$password', 'normal')";
+
+		return exe_sql($sql);
+
+	}
+
+?>
